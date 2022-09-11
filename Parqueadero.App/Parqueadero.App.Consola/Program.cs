f572adcd723d66dd9f9deff4a7667406ebd995f1
Luismarin2510 <opraedu@gmail.com>
2022-09-10 23:56:39 -0500

    adicion capa consola

---
+++ Parqueadero.App/Parqueadero.App.Consola/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Parqueadero.App.Dominio;
+using Parqueadero.App.Persistencia;
+namespace Parqueadero.App.Consola
+{
+    class program{
+        
+        static void Main (string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+             
+        }
+     
+     
+    }
+
+}
