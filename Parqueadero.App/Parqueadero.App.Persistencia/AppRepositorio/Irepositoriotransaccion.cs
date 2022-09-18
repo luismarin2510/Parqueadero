cfc15e1c83e5dc247daa5ea4ab7b397f163ded93
Luismarin2510 <opraedu@gmail.com>
2022-09-18 10:49:41 -0500

    irepo y repo transaccion

---
+++ Parqueadero.App/Parqueadero.App.Persistencia/AppRepositorio/Irepositoriotransaccion.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parqueadero.App.Persistencia.AppRepositorio
+{
+    public interface Irepositoriotransaccion
+    {
+        
+    }
+}
