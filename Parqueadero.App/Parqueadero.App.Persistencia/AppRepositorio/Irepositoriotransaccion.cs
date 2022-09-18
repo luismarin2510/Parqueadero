41ed4b79c585939d0e91fff838ea33ee780db6aa
Luismarin2510 <opraedu@gmail.com>
2022-09-18 11:21:07 -0500

    correcion comandos using y creacion irepo y repo

--- Parqueadero.App/Parqueadero.App.Persistencia/AppRepositorio/Irepositoriotransaccion.cs
+++ Parqueadero.App/Parqueadero.App.Persistencia/AppRepositorio/Irepositoriotransaccion.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using Parqueadero.App.Dominio;
 using Parqueadero.App.Persistencia;
 
---
+++ Parqueadero.App/Parqueadero.App.Persistencia/AppRepositorio/repositoriotiempocompleto.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System.Linq;
+using Parqueadero.App.Dominio;
+
+
+namespace Parqueadero.App.Persistencia
+{
+    public class repositoriotiempocompleto : Irepositoriotiempocompleto
+    {
+        
+    }
+}
