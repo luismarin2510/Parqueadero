5576c701b97121f1a5e2e57794ac9747ad36157b
Luismarin2510 <opraedu@gmail.com>
2022-09-18 09:53:44 -0500

    creacion irepo y repo lugar

---
+++ Parqueadero.App/Parqueadero.App.Persistencia/AppRepositorio/Irespositoriolugar.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Parqueadero.App.Dominio;
+using Parqueadero.App.Persistencia;
+
+namespace Parqueadero.App.Persistencia
+{
+    public interface Irespositoriolugar
+    {
+        
+    }
+}
---
+++ Parqueadero.App/Parqueadero.App.Persistencia/AppRepositorio/repositoriolugar.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using System.Linq;
+using Parqueadero.App.Dominio;
+
+namespace Parqueadero.App.Persistencia
+{
+    public class repositoriolugar
+    {
+        
+    }
+}
